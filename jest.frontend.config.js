37b23e718ba1e772413bafff4f6cd4b7a3ff8fe9
Wong Kai Li <53716255+garywongkai@users.noreply.github.com>
2025-03-10 11:24:55 +0800

    Update jest.frontend.config.js

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -22,12 +22,13 @@ export default {
   // only run these tests
   testMatch: ["<rootDir>/client/src/pages/*/*.test.js",
     "<rootDir>/client/src/pages/*.test.js", "<rootDir>/client/src/pages/Auth/*.test.js",
-    "<rootDir>/client/src/pages/admin/*.test.js", "<rootDir>/client/src/pages/*/*/*.test.js"],
+    "<rootDir>/client/src/pages/admin/*.test.js", "<rootDir>/client/src/pages/*/*/*.test.js", "<rootDir>/client/src/components/Form/*.test.js",
+ "<rootDir>/client/src/hooks/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: ["client/src/pages/*/*.js",  "client/src/pages/Auth/**",
-    "client/src/pages/admin/**", "client/src/pages/*.js", "client/src/pages/*/*/*.js"],
+    "client/src/pages/admin/**", "client/src/pages/*.js", "client/src/pages/*/*/*.js", "client/src/components/Form/**", "client/src/hooks/**"],
   coverageThreshold: {
     global: {
       lines: 25,

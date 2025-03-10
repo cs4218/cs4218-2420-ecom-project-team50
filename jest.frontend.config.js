47cc3533b354b3ce139a98952594eec373871880
pratham31012002 <pratham31012002@gmail.com>
2025-03-10 10:53:08 +0800

    Fix jest config files

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,15 +20,16 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: ["<rootDir>/client/src/pages/*/*.test.js",
+    "<rootDir>/client/src/pages/*.test.js", "<rootDir>/client/src/pages/*/*/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/pages/*/*.js", "client/src/pages/*.js", "client/src/pages/*/*/*.js"],
   coverageThreshold: {
     global: {
-      lines: 100,
-      functions: 100,
+      lines: 25,
+      functions: 25,
     },
   },
 };
